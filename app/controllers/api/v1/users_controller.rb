747d7525a7292b66a5f52533c25325e769fb3edc
yassimortensen <yassimortensen@gmail.com>
2018-02-21 18:13:41 -0500

    set up serializers and added file structure to controllers

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::UsersController < ApplicationController
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username)
+  end
+end
