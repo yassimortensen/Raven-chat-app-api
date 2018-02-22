048cd8384fe2d7116b3b57d911c49f3fd2f2163e
yassimortensen <yassimortensen@gmail.com>
2018-02-22 12:40:10 -0500

    fixed routing issue woth controller restructure and organized user serializer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :conversations, only:[:index, :create]
-  resources :users, only:[:create]
-  resources :messages, only: [:create]
-  mount ActionCable.server => '/cable'
+  namespace :api do
+    namespace :v1 do
+      resources :conversations, only:[:index, :create]
+      resources :users, only:[:index, :create]
+      resources :messages, only: [:create]
+      mount ActionCable.server => '/cable'
+    end
+  end
 end
